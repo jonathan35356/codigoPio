5766fbda86d6be5a21b198420315a0c3c22c0438
jonathan35356 <j.javier200211@outlook.com>
2024-10-07 09:05:48 -0500

    prompt

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-console.log("Primer programa con java");
+let nombre = prompt("Ingrese su nombre:")
+let ciudad = "Quito"
+let edad = prompt("Ingrese su edad")
+alert(`Hola ${nombre}, tienes ${edad} años y vives en ciudad ${ciudad} `)
